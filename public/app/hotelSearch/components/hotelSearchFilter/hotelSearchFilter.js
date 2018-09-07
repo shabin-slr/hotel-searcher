e6531b9d15422e8b27ee191f306e88aab750e60a
shabin.muhammed <shabin@gravitum.com>
2018-09-07 23:37:05 +0400

    front end

---
+++ public/app/hotelSearch/components/hotelSearchFilter/hotelSearchFilter.js
@@ -0,0 +1,49 @@
+(()=>{
+    class HotelSearchFilterController {
+        constructor(HotelSearchService){
+            this.HotelSearchService = HotelSearchService;
+            this.filter = {};
+            this.startDatePicker = {
+                isOpen : false
+            };
+            this.endDatePicker = {
+                isOpen : false
+            }
+        };
+
+        $onInit(){
+            console.log(this.hotels);
+            this.searchHotels();
+        };
+
+        searchHotels(){
+            /* if(this.filter.startDate){
+                if(this.filter.startDate && this.filter.startDate.match(/\d{4}-\d{2}-\d{2}/)){
+                    this.filter.startDate = this.filter.startDate.match(/\d{4}-\d{2}-\d{2}/)[0]
+                }
+            }
+            if(this.filter.endDate){
+                if(this.filter.endDate && this.filter.endDate.match(/\d{4}-\d{2}-\d{2}/)){
+                    this.filter.endDate = this.filter.endDate.match(/\d{4}-\d{2}-\d{2}/)[0]
+                }
+            } */
+            this.HotelSearchService.findHotels(this.filter)
+            .then(data=>{
+                this.hotels = data.data;
+            })
+        };
+
+        resetSearch(){
+            this.filter = {};
+            this.searchHotels();
+        }
+    };
+    angular.module("tajawal-test").component("hotelSearchFilter",{
+        templateUrl : "/app/hotelSearch/components/hotelSearchFilter/hotelSearchFilter.html",
+        controller : HotelSearchFilterController,
+        controllerAs : "$ctrl",
+        bindings : {
+            hotels : '='
+        }
+    })
+})();
